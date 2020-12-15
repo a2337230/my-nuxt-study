4ea7068e7dc831c22f8f8bc8ee576d48093c3ef5
qaz_xinlei <coolboyjrf1990@163.com>
2020-12-15 11:55:12 +0800

    22

---
+++ 01/store/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-15 11:32:23
+ * @LastEditTime: 2020-12-15 11:44:25
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \01\store\index.js
+ */
+export const actions = {
+  nuxtServerInit(store, context) {
+    console.log(store, context)
+  }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-## 第一天
-###生命周期
-  nuxtServerInit 
+# 第一天
+## 生命周期
+### nuxtServerInit  初始化时就会执行
+### middleware中间件 可以运行在nuxt.config.js中，也可以运行在layouts中，也可以运行在page中
+
