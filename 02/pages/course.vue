a7ec5ef0029f170646e88368713966257864b0ba
qaz_xinlei <coolboyjrf1990@163.com>
2020-12-17 11:04:50 +0800

    222

---
+++ 02/pages/course.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="course">
+    课程
+    <nuxt-link to="/course/1?a=1">分类1</nuxt-link>
+    <nuxt-link to="/course/2?a=2">分类2</nuxt-link>
+    <nuxt-link to="/course/3?a=3">分类3</nuxt-link>
+    <nuxt-link to="/course/4?a=4">分类4</nuxt-link>
+    <nuxt-link :to="{name: 'course-id', params: {id: 5}, query: {a: 1}}">分类5</nuxt-link>
+    <nuxt />
+  </div>
+</template>
---
+++ 02/pages/course/_id.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="detail">
+    <h3>分类</h3>
+  </div>
+</template>
